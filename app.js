9c1a5b1264b252ff7a609ad8d40fdba76d750307
alpesh <avasani@officebrain.com>
2017-04-28 15:56:16 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+let express = require('express');
+
+let app = express();
+
+app.get('/', function (req, res) {
+  res.send('hello world');
+});
+
+app.listen(process.env.PORT || 5055);
+
+module.exports = app;
