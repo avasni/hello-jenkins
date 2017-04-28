32bd902c9a7b5bc5e840bb47d0d029dba89e7d93
alpesh <avasani@officebrain.com>
2017-04-28 16:42:44 +0530

    change port

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 let express = require('express');
-
 let app = express();
 
 app.get('/', function (req, res) {
   res.send('hello world');
 });
 
-app.listen(process.env.PORT || 5055);
+app.listen(process.env.PORT || 5055, function () {
+  console.log('Example app listening on port 5055!')
+});
 
 module.exports = app;
